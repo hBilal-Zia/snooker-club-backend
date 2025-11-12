c55ffb6f8f5d53e4d4b66d2e60488b4984958360
hBilal-Zia <hbilalzia92@gmail.com>
2025-11-12 23:19:12 +0500

    Changed: End Session API to Patch from Get

--- src/routes/session.route.ts
+++ src/routes/session.route.ts
@@ -7,7 +7,7 @@ const sessionRouter = express.Router();
 sessionRouter.post("/",verifyAdmin, isAuthorize(['super admin', 'admin']), SessionController.createSession);
 sessionRouter.get("/",verifyAdmin, isAuthorize(['super admin', 'admin']), SessionController.getSessions);
 sessionRouter.get("/:sessionId",verifyAdmin, isAuthorize(['super admin', 'admin']), SessionController.getSession);
-sessionRouter.get("/:sessionId/end",verifyAdmin, isAuthorize(['super admin', 'admin']), SessionController.endSession);
+sessionRouter.patch("/:sessionId/end",verifyAdmin, isAuthorize(['super admin', 'admin']), SessionController.endSession);
 sessionRouter.patch("/:sessionId/paid",verifyAdmin, isAuthorize(['super admin', 'admin']), SessionController.updateSessionPaidStatus);
 
 export default sessionRouter;
