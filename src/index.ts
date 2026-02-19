84be46156d574abc666dcd1a857a147419bdbcdc
hBilal-Zia <hbilalzia92@gmail.com>
2026-02-20 01:00:25 +0500

    Added: Logging middlewear Morgan

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ import config from "./config/config";
 import { connectDB } from "./config/database";
 import adminRouter from "./routes/route"
 import cors from "cors";
+import morgan from "morgan"
 
 const app = express();
 const PORT = config.port;
 
 app.use(express.json());
 app.use(cors())
+app.use(morgan('dev'))
 app.use("/api/v1/admin", adminRouter)
 
 app.get("/health-check", (req: Request, res: Response<ApiResponse<{}>>) => {
