d3f194daf7311670e515c12216bce5c17732bbef
hBilal-Zia <hbilalzia92@gmail.com>
2026-02-23 01:32:26 +0500

    Removed min length on Password in Admin Login API

--- src/validatiors/auth.validator.ts
+++ src/validatiors/auth.validator.ts
@@ -7,9 +7,8 @@ export const loginSchema = Joi.object<LoginRequestDTO>({
         "string.email": "Email must be a valid email address",
         "any.required": "Email is required",
     }),
-    password: Joi.string().min(6).required().messages({
+    password: Joi.string().required().messages({
         "string.empty": "Password cannot be empty",
-        "string.min": "Password must be at least 6 characters long",
         "any.required": "Password is required",
     }),
 });
