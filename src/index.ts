acb5e4cded19e23c2a6126d11132044e01f41aca
hBilal-Zia <hbilalzia92@gmail.com>
2026-06-09 20:33:46 +0500

    add: helmet middlewear

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import { connectDB } from "./config/database";
 import adminRouter from "./routes/route";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 const PORT = config.port;
 
+app.use(helmet())
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
